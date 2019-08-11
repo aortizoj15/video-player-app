b738ac395f41a96cb400b94ee33e6b151d2b3cb2
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2019-08-11 00:04:16 -0700

    Updates

---
+++ precache-manifest.56020c346eddb7f785d3cb3c0625fa0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32748b5ec4b7005bc72d1dd0b22e6b71",
+    "url": "/video-player-app/index.html"
+  },
+  {
+    "revision": "200a6b89790b19404f87",
+    "url": "/video-player-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8540cbd1a90eebe0dac0",
+    "url": "/video-player-app/static/js/2.bc044a8a.chunk.js"
+  },
+  {
+    "revision": "200a6b89790b19404f87",
+    "url": "/video-player-app/static/js/main.e7645ab9.chunk.js"
+  },
+  {
+    "revision": "0427e53fcdcfcb2bdd4e",
+    "url": "/video-player-app/static/js/runtime~main.b5ccd678.js"
+  }
+]);
