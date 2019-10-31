9b2ae4d6f80c4c2699762af98448baaa7d9fdc14
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2019-10-30 17:58:10 -0700

    Changed header paragraph

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -5,8 +5,8 @@ const Header: any = () => {
   return (
     <div className="container">
       <p>
-        Is there a YouTube video that just has way too many ads? Watch it now
-        ad-free! All you gotta do is search for it. Enjoy!
+        Is there a YouTube video that just has way too many ads? Search for it
+        here and enjoy ad-free!
       </p>
     </div>
   );
